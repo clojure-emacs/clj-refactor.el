03dba3af897302cb1e01ee85959658937dc35b13
Alex Baranosky <alexander.baranosky@gmail.com>
2014-01-26 15:14:41 -0800

    paredit-backward-up 99 may be safer than searchb-backward

--- clj-refactor.el
+++ clj-refactor.el
@@ -216,7 +216,7 @@
 (defun cljr--goto-ns ()
   (goto-char (point-min))
   (if (re-search-forward clojure-namespace-name-regex nil t)
-      (search-backward "(ns")
+      (ignore-errors (paredit-backward-up 99))
     (error "No namespace declaration found")))
 
 (defun cljr--insert-in-ns (type)
