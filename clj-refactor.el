d79893b4e416b0674d5b023c6c87bc75847db365
Lars Andersen <expez@expez.com>
2015-05-16 19:11:36 +0200

    Bump version in package header to 1.1.0-SNAPSHOT

--- clj-refactor.el
+++ clj-refactor.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2012-2015 Magnar Sveen <magnars@gmail.com>
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
-;; Version: 1.0.5
+;; Version: 1.1.0-SNAPSHOT
 ;; Keywords: convenience
 ;; Package-Requires: ((s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (cider "0.8.1"))
 
