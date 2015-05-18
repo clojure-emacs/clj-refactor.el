d4c3bff0f5b921472f63743cbbd7705f9ae6413a
Lars Andersen <lars.andersen@gjensidige.no>
2015-05-18 09:13:30 +0200

    Drop support for emacs versions below 24.3
    clojure-mode has dropped support for emacs versions older than this.

--- clj-refactor.el
+++ clj-refactor.el
@@ -5,7 +5,7 @@
 ;; Author: Magnar Sveen <magnars@gmail.com>
 ;; Version: 1.0.5
 ;; Keywords: convenience
-;; Package-Requires: ((s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (cider "0.8.1"))
+;; Package-Requires: ((emacs "24.3") (s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (cider "0.8.1"))
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
