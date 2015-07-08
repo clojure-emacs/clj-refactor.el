ce7b58f5eb7499ed798ba010422ee6c202bce1bf
Lars Andersen <expez@expez.com>
2015-07-08 12:02:06 +0200

    [Fix #187] Depend on stable edn.el version

--- clj-refactor.el
+++ clj-refactor.el
@@ -8,7 +8,7 @@
 ;;         Benedek Fazekas
 ;; Version: 1.1.0
 ;; Keywords: convenience, clojure, cider
-;; Package-Requires: ((emacs "24.3") (s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (cider "0.9.1") (edn "1.1.1"))
+;; Package-Requires: ((emacs "24.3") (s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (cider "0.9.1") (edn "1.1.2"))
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
