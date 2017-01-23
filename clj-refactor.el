05fd0c7b35b6eee4a3546780dde532866bbd2a0e
Toni Reina <areina0@gmail.com>
2017-01-23 13:30:35 +0100

    Replace `-distinct` by `seq-uniq`

--- clj-refactor.el
+++ clj-refactor.el
@@ -8,7 +8,7 @@
 ;;         Benedek Fazekas <benedek.fazekas@gmail.com>
 ;; Version: 2.3.0-SNAPSHOT
 ;; Keywords: convenience, clojure, cider
-;; Package-Requires: ((emacs "24.4") (s "1.8.0") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (clojure-mode "5.6.1") (cider "0.11.0") (edn "1.1.2") (inflections "2.3") (hydra "0.13.2"))
+;; Package-Requires: ((emacs "24.4") (s "1.8.0") (seq "2.19") (dash "2.4.0") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (clojure-mode "5.6.1") (cider "0.11.0") (edn "1.1.2") (inflections "2.3") (hydra "0.13.2"))
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
@@ -30,6 +30,7 @@
 ;;; Code:
 
 (require 'dash)
+(require 'seq)
 (require 'clj-refactor-compat)
 (require 's)
 (require 'yasnippet)
@@ -1704,7 +1705,7 @@ See: https://github.com/clojure-emacs/clj-refactor.el/wiki/cljr-destructure-keys
     (when (looking-back "\\s_\\|\\sw" 3)
       (paredit-backward))
     (kill-sexp)
-    (insert "{:keys [" (s-join " " (-distinct (reverse symbols))) "]"
+    (insert "{:keys [" (s-join " " (seq-uniq (reverse symbols))) "]"
             (if include-as (concat " :as " symbol) "") "}")))
 
 ;; ------ Cycling ----------
