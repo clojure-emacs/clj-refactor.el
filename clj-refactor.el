50d2d8aad5e0bd8002173b300f8419d72ceab7af
Bozhidar Batsov <bozhidar@batsov.com>
2019-06-18 10:16:51 +0300

    Bump the development version

--- clj-refactor.el
+++ clj-refactor.el
@@ -6,7 +6,7 @@
 ;; Author: Magnar Sveen <magnars@gmail.com>
 ;;         Lars Andersen <expez@expez.com>
 ;;         Benedek Fazekas <benedek.fazekas@gmail.com>
-;; Version: 2.4.0
+;; Version: 2.5.0-snapshot
 ;; Keywords: convenience, clojure, cider
 ;; Package-Requires: ((emacs "25.1") (seq "2.19") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (clojure-mode "5.6.1") (cider "0.17.0") (edn "1.1.2") (inflections "2.3") (hydra "0.13.2"))
 
