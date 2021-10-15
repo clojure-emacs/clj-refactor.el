a5e1953753310e5e715e8e1e293181bf61521440
vemv <vemv@users.noreply.github.com>
2021-10-15 10:21:08 +0200

    Bump the minimum required parseedn version
    See also: https://github.com/clojure-emacs/cider/pull/3072

--- clj-refactor.el
+++ clj-refactor.el
@@ -10,7 +10,7 @@
 ;; Version: 3.0.0-alpha13
 ;; Keywords: convenience, clojure, cider
 
-;; Package-Requires: ((emacs "26.1") (seq "2.19") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (clojure-mode "5.9") (cider "1.0") (parseedn "1.0.4") (inflections "2.3") (hydra "0.13.2"))
+;; Package-Requires: ((emacs "26.1") (seq "2.19") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (clojure-mode "5.9") (cider "1.0") (parseedn "1.0.46) (inflections "2.3") (hydra "0.13.2"))
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
