3e5dca8e6869d5d085cee3295abceffef8d12c9f
Charles Comstock <dgtized@gmail.com>
2022-07-08 23:59:58 -0500

    Add simple test for cljr--namespace-aliases
    Shows how prefixes and namespaces can be shared across a clj and cljs namespace.

--- tests/unit-test.el
+++ tests/unit-test.el
@@ -1,5 +1,6 @@
 (require 'paredit)
 (require 'clj-refactor)
+(require 'buttercup)
 
 ;; NOTE: please remember, without an `it` block, your tests will not be run!
 ;; You can learn about Buttercup's syntax here:
@@ -9,3 +10,19 @@
   (it "returns the ns name of its argument"
     (expect (cljr--ns-name "com.corp.foo") :to-equal "foo")
     (expect (cljr--ns-name "foo") :to-equal "foo")))
+
+(describe "cljr--namespace-aliases"
+  (it "reduces to a unique list from middleware"
+    (spy-on 'cljr--call-middleware-for-namespace-aliases
+            :and-return-value
+            (parseedn-read-str
+             "{:clj  {t (clojure.test) set (clojure.set) sut (alpha shared)}
+               :cljs {t (cljs.test) set (clojure.set) sut (beta shared)}}"))
+    (expect (cljr--namespace-aliases)
+            :to-equal
+            '((t clojure.test (:clj))
+              (sut alpha (:clj))
+              (t cljs.test (:cljs))
+              (set clojure.set (:clj :cljs))
+              (sut beta (:cljs))
+              (sut shared (:clj :cljs))))))
