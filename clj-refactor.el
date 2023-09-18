ed4168b1457cc3b145101a88f63291e6b7fce091
vemv <vemv@users.noreply.github.com>
2023-09-18 12:14:49 +0200

    x

--- clj-refactor.el
+++ clj-refactor.el
@@ -7,7 +7,7 @@
 ;;         Lars Andersen <expez@expez.com>
 ;;         Benedek Fazekas <benedek.fazekas@gmail.com>
 ;;         Bozhidar Batsov <bozhidar@batsov.dev>
-;; Version: 3.9.3
+;; Version: 3.9.4
 ;; Keywords: convenience, clojure, cider
 
 ;; Package-Requires: ((emacs "26.1") (seq "2.19") (yasnippet "0.6.1") (paredit "24") (multiple-cursors "1.2.2") (clojure-mode "5.16.1") (cider "1.7.0") (parseedn "1.1.0") (inflections "2.6") (hydra "0.13.2"))
@@ -3429,7 +3429,8 @@ See: https://github.com/clojure-emacs/clj-refactor.el/wiki/cljr-inline-symbol"
             (cljr--post-command-message "No occurrences of '%s' found.  Deleted the definition." symbol)))))
     (cljr--indent-defun)))
 
-(defvar cljr-version)
+(defconst cljr-version "3.9.4"
+  "The current version of clj-refactor.")
 
 (defun cljr--pkg-version ()
   "Extract the package version from its package metadata."
